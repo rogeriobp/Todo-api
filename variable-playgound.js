10069db9c8c0c8101f95b74a7dcb313b4afb66ca
Rogerio Peyroton <rogeriobp@gmail.com>
2016-08-31 01:03:51 -0300

    add variable playground area

---
+++ variable-playgound.js
@@ -0,0 +1,27 @@
+//var person = {
+//    name: 'Andrew',
+//    age: 21
+//};
+//
+//function updatePerson (obj) {
+////    obj = {
+////        name: 'Andrew',
+////        age: 24
+////    };
+//    obj.age = 24;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+// Array Examples
+var grades = [50,37];
+
+function addGrade (array) {
+    array.push(55);
+    debugger;
+//    array = [50,37,55];
+}
+
+addGrade(grades);
+console.log(grades);
