7b1f82f573318284fdefc46655b048d8c291e5bb
Rogerio Peyroton <rogeriobp@gmail.com>
2016-09-08 12:35:33 -0300

    added todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,45 +19,35 @@ var Todo = sequelize.define('todo', {
     }
 });
 
+var User = sequelize.define('user', {
+    email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
-//    force: true
+    force:true
 }).then(function(){
     console.log('Everything is synced');
     
-    Todo.findById(3).then(function(todo){
-        if (todo === null){
-            console.log('Id not found');
-        } else {
-            console.log(todo.toJSON());    
-        }
-    }).catch(function(e){
-        console.log(e);
-    });
+    User.findById(1).then(function(user){
+        user.getTodos().then(function(todos){
+            todos.forEach(function(todo){
+                console.log(todo.toJSON());
+            })
+        })
+    })
     
-//    Todo.create({
-//        description: 'Take out trash'
-//    }).then(function(todo) {
-//        return Todo.create({
-//            description: 'Clean office'
-//        });
-//    }).then(function(){
-////        return Todo.findById(1);
-//        return Todo.findAll({
-//            where: {
-//                description: {
-//                    $like: '%Office%'
-//                }
-//            }
-//        });
-//    }).then(function(todos){
-//        if (todos) {
-//            todos.forEach(function(todo){
-//                console.log(todo.toJSON());    
-//            });
-//        } else {
-//            console.log('No todos found');
-//        }
-//    }).catch(function(e){
-//        console.log(e);
-//    });
+    User.create({
+        email: 'test@test.com'
+    }).then(function(){
+        return Todo.create({
+            description: 'Clean yard'
+        });
+    }).then(function(todo){
+        User.findById(1).then(function(user){
+            user.addTodo(todo);
+        });
+    });
 });
